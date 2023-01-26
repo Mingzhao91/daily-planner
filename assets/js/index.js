567be7447b8ab0f988fa3d252172f8ab2f9b3570
Mingzhao91 <edwardwhite7@hotmail.com>
2023-01-26 20:18:48 +0000

    create timeblock and its subsequent elements

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,9 +11,62 @@ function showCurrentDate() {
   });
 }
 
+function getTimeblockHourEl(hour) {
+  // create span tag to show offcie hour
+  const hourEl = $("<span>");
+  // get hour string in format of 1PM
+  const hourShort = moment().hour(hour).format("hA");
+
+  hourEl.text(hourShort);
+  return hourEl;
+}
+
+function getTimeblockInputEl(hour) {
+  // create an input tag to allow user to type in an event
+  const eventInputEl = $("<input>");
+  return eventInputEl;
+}
+
+function getTimeblockSaveBtnEl() {
+  // create a save button to allow user to save an event
+  const saveBtn = $("<button>");
+  const saveIconEl = $(`<i class="far fa-save">`);
+  saveBtn.append(saveIconEl);
+  return saveBtn;
+}
+
+function getTimeblock(hour) {
+  // create a timeblock element
+  const timeblockEl = $("<div>");
+
+  // get all elements inside a timeblock
+  const hourEl = getTimeblockHourEl(hour);
+  const inputEl = getTimeblockInputEl(hour);
+  const saveBtnEl = getTimeblockSaveBtnEl();
+
+  // append individual elements into timeblock element
+  timeblockEl.append(hourEl);
+  timeblockEl.append(inputEl);
+  timeblockEl.append(saveBtnEl);
+
+  return timeblockEl;
+}
+
+function buildTimeblocks(startingHour, endingHour) {
+  console.log("hello");
+  // loop throught hours between starting hour and ending hour
+  for (let hour = startingHour; hour <= endingHour; hour++) {
+    console.log("hour: ", hour);
+
+    // append timeblock in timeblocks container
+    $("#timeblocks").append(getTimeblock(hour));
+  }
+}
+
 // start the application
 function start() {
   showCurrentDate();
+  buildTimeblocks(OFFICE_HOUR_START_IN_24, OFFICE_HOUR_END_IN_24);
 }
 
 start();
